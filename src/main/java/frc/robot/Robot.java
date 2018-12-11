d3f323cf8b14310d55c7d1a0fd7c9a428343aa16
FTC7288 <ftcteam7288@gmail.com>
2018-12-11 16:23:26 -0600

    Updated Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,92 +7,93 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
+
 public class Robot extends IterativeRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+	Joystick stick = new Joystick(0);
+	
+	WPI_TalonSRX LeftMotor = new WPI_TalonSRX(1);
+	WPI_TalonSRX RightMotor = new WPI_TalonSRX(2);
+	WPI_TalonSRX LiftMotor = new WPI_TalonSRX(0);
+	WPI_TalonSRX AngleMotor = new WPI_TalonSRX(3);
+	Encoder LeftEncoder = new Encoder(8,9);
+	Encoder RightEncoder = new Encoder(7,6);
+	Encoder LiftEncoder = new Encoder(4,5);
+	Encoder AngleEncoder = new Encoder(2,3);
+	
+	Servo AngleServo = new Servo(0);
+	Servo LiftServo = new Servo(1);
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.addDefault("Default Auto", kDefaultAuto);
-    m_chooser.addObject("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
+	
+	DifferentialDrive drive = new DifferentialDrive(LeftMotor, RightMotor);
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // autoSelected = SmartDashboard.getString("Auto Selector",
-    // defaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
+	@Override
+	public void robotInit() {
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
+	}
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-  }
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+	@Override
+	public void autonomousInit() {
+
+	}
+
+
+	@Override
+	public void autonomousPeriodic() {
+
+	}
+
+
+	@Override
+	public void teleopPeriodic() {
+		drive.arcadeDrive(-stick.getRawAxis(1), stick.getRawAxis(0));
+		
+		if(stick.getPOV() == 0) {
+			LiftMotor.set(1);
+		}else if(stick.getPOV() == 180) {
+			LiftMotor.set(-1);
+		}else if(stick.getPOV() == -1) {
+      LiftMotor.set(0);
+		}
+		
+		if(stick.getRawButton(4)) {
+			AngleMotor.set(0.25);
+		}else if(stick.getRawButton(1)) {
+			AngleMotor.set(-0.25);
+		}else {
+			AngleMotor.set(0);
+		}
+		
+		if(stick.getRawButton(2)) {
+			AngleServo.set(0);
+		}else if(stick.getRawButton(3)) {
+			AngleServo.set(1);
+		}
+		
+		if(stick.getPOV() == 90) {
+			LiftServo.set(0);
+		}else if(stick.getPOV() == 270) {
+			LiftServo.set(1);
+		}
+		
+		SmartDashboard.putNumber("Encoder Left", LeftEncoder.get());
+		SmartDashboard.putNumber("Encoder Right", RightEncoder.get());
+		SmartDashboard.putNumber("Encoder Lift", LiftEncoder.get());
+		SmartDashboard.putNumber("Encoder Angle", AngleEncoder.get());
+		
+	}
+
+	@Override
+	public void testPeriodic() {
+	}
 }
